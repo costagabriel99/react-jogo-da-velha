76f5a2892f4ad184c81f7ac7a44d15a83a1090fb
Gabriel Costa <22gabrielgc@gmail.com>
2023-02-20 15:33:51 -0300

    Criado componente Game

--- src/components/gamePage/GamePage.jsx
+++ src/components/gamePage/GamePage.jsx
@@ -1,9 +1,11 @@
 import Header from "../header/Header"
+import Game from '../game/Game'
 
 function GamePage () {
     return (
         <div className="container">
             <Header />
+            <Game />
         </div>
     )
 }
