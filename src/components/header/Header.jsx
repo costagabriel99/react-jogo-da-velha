404e3e840b84b2d880a7ee0265f5c1b03275480a
Gabriel Costa <22gabrielgc@gmail.com>
2023-02-20 15:06:37 -0300

    Criado componente de Icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import styles from './Header.module.css'
 import Title from "../title/Title"
 import Subtitle from "../subtitle/Subtitle"
+import Icon from '../icon/Icon'
 
 function Header () {
     return (
         <div className={styles.header}>
         <Title>Jogo da Velha</Title>
         <Subtitle>Criado por Gabriel Costa</Subtitle>
+         <div className={styles.iconContent}>
+            <Icon iconName="github" link="https://github.com/costagabriel99"/>
+         </div>
         </div>
         
     )
